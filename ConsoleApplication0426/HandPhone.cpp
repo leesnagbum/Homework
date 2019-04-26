409563abdeec4b7ccd3709e0f93a88e43fda8712
부수기재 <qronia@gmail.com>
2019-04-26 18:20:27 +0900

    수정된 내용입니다.
    확인바람

--- ConsoleApplication0426/HandPhone.cpp
+++ ConsoleApplication0426/HandPhone.cpp
@@ -6,7 +6,11 @@
 
 void HandPhone::Input()
 {
-	std::cout << "모델명:" << model << std::endl;
+
+
+	std::cout << "모델명:";
+	std::cin >> model;
+
 	std::cout << "회사명:" << company << std::endl;
 	std::cout << "색 상:" << color << std::endl;
 	std::cout << "전원 상태:" << power << std::endl;
@@ -19,31 +23,25 @@ void HandPhone::Print()
 	std::cout << "상품의 회사명은 " << company << std::endl;
 	std::cout << "상품의 색상은 " << color << std::endl;
 
-	if (power == "on" || power == "ON")
+	if (isPower)
 	{
-		PowerOn();
+		std::cout << "전원이 켜져있습니다." << std::endl;
 	}
 	else
 	{
-		PowerOff();
+		std::cout << "전원이 꺼져있습니다." << std::endl;
 	}
 }
 
-void HandPhone::PowerOn()
-{
+// 전원을 끄고 키게 한다.
+void HandPhone::TogglePower() { isPowered = !isPowered; } // 토글. 꺼져있을때에는 키고 켜져있을때에는 끈다.
+void HandPhone::PowerOn() { isPowered = true; }
+void HandPhone::PowerOff() { isPowered = false; }
 
-}
-void HandPhone::PowerOff()
-{
 
-}
-void HandPhone::CameraYes()
-{
 
-}
-void HandPhone::CameraNo()
-{
-
-}
+// 카메라 여부 조작.
+void HandPhone::CameraYes(){ hasCamera = true; }
+void HandPhone::CameraNo() { hasCamera = false; }
 
 
--- ConsoleApplication0426/HandPhone.h
+++ ConsoleApplication0426/HandPhone.h
@@ -11,24 +11,30 @@ class HandPhone
 public:
 
 	HandPhone();
-	~HandPhone();
+	~HandPhone() = default;
 
 	void Input();
 	void Print();
+	void TogglePower();
+
 	void PowerOn();
 	void PowerOff();
+
 	void CameraYes();
 	void CameraNo();
+
 
 private:
 	std::string model;
 	std::string company;
 	std::string color;
 
-	bool on;
-	bool install;
-	char power;
-	char camera;
+	bool isPowered; // 전원이 켜져있는가?
+	bool hasCamera; // 카메라가 있는가?
+
+
+	// 이 변수는 ~~를 합니다. ~~~를 나타냅니다.
+	
 
 };
 
